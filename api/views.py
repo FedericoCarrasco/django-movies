76e40b4d2c89c58120db26db04db9afebb44c770
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-13 18:21:24 -0300

    Add genre viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Genre, Character, Movie
+from rest_framework import viewsets, permissions
+from .serializers import GenreSerializer, CharacterSerializer, MovieSerializer
 
 # Create your views here.
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [permissions.AllowAny]
+
