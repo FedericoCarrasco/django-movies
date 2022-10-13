fc8cc0e931154b3560e04d2bbd273ca632c2661f
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-13 18:35:00 -0300

    fix tuples in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -5,17 +5,17 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ('id', 'name', 'image')
-        read_only_fields = ('id')
+        read_only_fields = ('id', )
 
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ('id', 'name', 'image', 'age', 'weight', 'story')
-        read_only_fields = ('id')
+        read_only_fields = ('id', )
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ('id', 'title', 'image', 'release_date', 'rating', 'genre', 'characters')
-        read_only_fields = ('id')
+        read_only_fields = ('id', )
 
