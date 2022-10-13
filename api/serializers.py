26d52d221273fd8d314418d8ac9255d45d859c8f
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-13 18:15:42 -0300

    Add movie serializer

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,9 @@ class CharacterSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'image', 'age', 'weight', 'story')
         read_only_fields = ('id')
 
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'image', 'release_date', 'rating', 'genre', 'characters')
+        read_only_fields = ('id')
+
