3f789c17b974d8292667f65f9357b476d7c556d6
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-13 17:54:32 -0300

    Add character model

--- django_movies/api/models.py
+++ django_movies/api/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=200)
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    age = models.PositiveSmallIntegerField(null=True)
+    weight = models.DecimalField(decimal_places=2, max_digits=4, null=True)
+    story = models.TextField(null=True)
 
