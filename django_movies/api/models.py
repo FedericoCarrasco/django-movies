e17992809b52de14e3ca75abf3a068325b4225e3
Federico Carrasco <federicogcarrasco@gmail.com>
2022-10-13 17:58:21 -0300

    Add movie model

--- django_movies/api/models.py
+++ django_movies/api/models.py
@@ -14,3 +14,11 @@ class Character(models.Model):
     weight = models.DecimalField(decimal_places=2, max_digits=4, null=True)
     story = models.TextField(null=True)
 
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    release_date = models.DateField(null=True)
+    rating = models.DecimalField(decimal_places=1, max_digits=2, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
+    characters = models.ManyToManyField(Character)
+
